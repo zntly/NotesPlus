e0e7a1b4c02c40d54dea779e08ba27ca2aa33e3a
zntly <83675710+zntly@users.noreply.github.com>
2024-12-07 14:14:43 -0500

    Update Utils.cs

--- Source/NotesPlus/Utils.cs
+++ Source/NotesPlus/Utils.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using BetterTOS2;
+using Server.Shared.State;
+using Services;
 using SML;
 
 namespace NotesPlus
@@ -7,13 +9,13 @@ namespace NotesPlus
 	// Token: 0x02000006 RID: 6
 	public static class Utils
 	{
-		// Token: 0x0600001D RID: 29 RVA: 0x00002193 File Offset: 0x00000393
+		// Token: 0x0600001D RID: 29
 		public static bool BTOS2Exists()
 		{
 			return ModStates.IsEnabled("curtis.tuba.better.tos2");
 		}
 
-		// Token: 0x0600001E RID: 30 RVA: 0x00003200 File Offset: 0x00001400
+		// Token: 0x0600001E RID: 30
 		public static bool IsBTOS2()
 		{
 			bool result;
@@ -28,10 +30,22 @@ namespace NotesPlus
 			return result;
 		}
 
-		// Token: 0x0600001F RID: 31 RVA: 0x0000219F File Offset: 0x0000039F
+		// Token: 0x0600001F RID: 31
 		private static bool IsBTOS2Bypass()
 		{
 			return Utils.BTOS2Exists() && BTOSInfo.IS_MODDED;
+		}
+
+		// Token: 0x06000023 RID: 35
+		public static bool IsPandora()
+		{
+			return Utils.IsBTOS2() && Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains((Role)222);
+		}
+
+		// Token: 0x06000024 RID: 36
+		public static bool IsCompliance()
+		{
+			return Utils.IsBTOS2() && Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains((Role)221);
 		}
 	}
 }
