6722bc456cea175747a4064c5e14bfcabb3d377a
zntly <83675710+zntly@users.noreply.github.com>
2024-12-07 21:47:25 -0500

    Update Settings.cs

--- Source/NotesPlus/Settings.cs
+++ Source/NotesPlus/Settings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using SML;
 
@@ -9,7 +9,7 @@ namespace NotesPlus
 	public class Settings
 	{
 		// Token: 0x17000001 RID: 1
-		// (get) Token: 0x06000002 RID: 2 RVA: 0x00002058 File Offset: 0x00000258
+		// (get) Token: 0x06000002 RID: 2
 		public ModSettings.DropdownSetting SetFactionSettings
 		{
 			get
@@ -17,7 +17,7 @@ namespace NotesPlus
 				return new ModSettings.DropdownSetting
 				{
 					Name = "Show Faction Color",
-					Description = "When will a role show its faction color (if it shouldn't, it will show as grey)",
+					Description = "When will a role show its faction color (if it shouldn't, it will show as gray)\nAlways - Roles will always use a faction color\nOnly Marked - Roles will only use their faction color if you mark them with an asterisk (*)\nOnly Roles - Roles will use their faction color, alignments will not\nOnly On Override - Roles will only use a faction color if a faction override is specified (e.g. [[#24,2]] for Coven-Vigilante)\nNever - Roles will always be gray and never use a faction color",
 					Options = this.FactionSettings,
 					AvailableInGame = false,
 					Available = true
@@ -26,11 +26,12 @@ namespace NotesPlus
 		}
 
 		// Token: 0x04000001 RID: 1
-		private readonly List<string> FactionSettings = new List<string>(4)
+		private readonly List<string> FactionSettings = new List<string>(5)
 		{
 			"Always",
 			"Only Marked",
 			"Only Roles",
+			"Only On Override",
 			"Never"
 		};
 	}
