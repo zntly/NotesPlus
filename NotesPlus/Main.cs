e78bc63d30985756430c9f3f439cceabb0d841b0
zntly <83675710+zntly@users.noreply.github.com>
2025-02-16 12:05:34 -0500

    Add files via upload

---
+++ NotesPlus/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SML;
+
+namespace NotesPlus
+{
+	// Token: 0x02000004 RID: 4
+	[Mod.SalemMod]
+	public class Main
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x0000208F File Offset: 0x0000028F
+		public void Start()
+		{
+			Console.WriteLine("anyone can be an enchanter if they try hard enough");
+		}
+	}
+}
