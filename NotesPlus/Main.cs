b37b9d1bc83eb2062c1a955f3cfd5aee191fa0fe
zntly <83675710+zntly@users.noreply.github.com>
2025-03-04 18:13:51 -0500

    Add files via upload

---
+++ NotesPlus/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SML;
+
+namespace NotesPlus
+{
+	// Token: 0x02000004 RID: 4
+	[Mod.SalemMod]
+	public class Main
+	{
+		// Token: 0x06000004 RID: 4
+		public void Start()
+		{
+			Console.WriteLine("anyone can be an enchanter if they try hard enough");
+			try
+			{
+				bool @bool = ModSettings.GetBool("Traitor Detections", "synapsium.notes.plus");
+				if (!ModSettings.GetBool("Did Update Traitor Detections", "synapsium.notes.plus"))
+				{
+					ModSettings.SetBool("Faction Abbreviations", @bool, "synapsium.notes.plus");
+					ModSettings.SetBool("Did Update Traitor Detections", true, "synapsium.notes.plus");
+				}
+			}
+			catch
+			{
+				Console.WriteLine("maybe you're just better off as a plaguebearer");
+			}
+		}
+	}
+}
