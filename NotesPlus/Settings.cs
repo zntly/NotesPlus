0c3e31902702444febfe3950b5c3d262ab4a8dda
zntly <83675710+zntly@users.noreply.github.com>
2025-03-15 20:05:28 -0400

    Add files via upload

---
+++ NotesPlus/Settings.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using SML;
+
+namespace NotesPlus
+{
+	// Token: 0x02000003 RID: 3
+	[DynamicSettings]
+	public class Settings
+	{
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000002 RID: 2
+		public ModSettings.DropdownSetting SetFactionSettings
+		{
+			get
+			{
+				return new ModSettings.DropdownSetting
+				{
+					Name = "Show Faction Color",
+					Description = "When will a role show its faction color (if it shouldn't, it will show as gray)\nAlways - Roles will always use a faction color\nOnly Marked - Roles will only use their faction color if you mark them with an asterisk (*)\nOnly Roles - Roles will use their faction color, alignments will not\nOnly On Override - Roles will only use a faction color if a faction override is specified (e.g. [[#24,2]] for Coven-Vigilante)\nNever - Roles will always be gray and never use a faction color",
+					Options = this.FactionSettings,
+					AvailableInGame = false,
+					Available = true
+				};
+			}
+		}
+
+		// Token: 0x17000002 RID: 2
+		// (get) Token: 0x06000004 RID: 4
+		public ModSettings.CheckboxSetting DidUpdateTraitorDetections
+		{
+			get
+			{
+				return new ModSettings.CheckboxSetting
+				{
+					Name = "Did Update Traitor Detections",
+					Description = "This is an internal setting used to tell if the player has already had their old setting migrated to the new one (Traitor Detections -> Faction Abbreviations).",
+					DefaultValue = false,
+					AvailableInGame = false,
+					Available = false
+				};
+			}
+		}
+
+		// Token: 0x17000003 RID: 3
+		// (get) Token: 0x06000005 RID: 5
+		public ModSettings.CheckboxSetting AdditionalNotes
+		{
+			get
+			{
+				return new ModSettings.CheckboxSetting
+				{
+					Name = "Additional Notes",
+					Description = "Have additional notes appear next to a person's name besides just a role - encase what you would like to show in [single brackets]",
+					DefaultValue = true,
+					AvailableInGame = false,
+					Available = true
+				};
+			}
+		}
+
+		// Token: 0x17000004 RID: 4
+		// (get) Token: 0x06000006 RID: 6
+		public ModSettings.ColorPickerSetting AdditionalNotesColor
+		{
+			get
+			{
+				return new ModSettings.ColorPickerSetting
+				{
+					Name = "Additional Notes Color",
+					Description = "The color of additional notes",
+					DefaultValue = "#FFFFFF",
+					AvailableInGame = false,
+					Available = ModSettings.GetBool("Additional Notes")
+				};
+			}
+		}
+
+		// Token: 0x17000009 RID: 9
+		// (get) Token: 0x0600003B RID: 59
+		public ModSettings.DropdownSetting AdditionalNotesStyle
+		{
+			get
+			{
+				return new ModSettings.DropdownSetting
+				{
+					Name = "Additional Notes Style",
+					Description = "The way the additional notes look next to one's name (this does not change how you input additional notes, only how it appears in the playerlist)",
+					Options = this.AdditionalNotesStyleList,
+					AvailableInGame = true,
+					Available = ModSettings.GetBool("Additional Notes")
+				};
+			}
+		}
+
+		// Token: 0x04000001 RID: 1
+		private readonly List<string> FactionSettings = new List<string>(5)
+		{
+			"Always",
+			"Only Marked",
+			"Only Roles",
+			"Only On Override",
+			"Never"
+		};
+
+		// Token: 0x04000017 RID: 23
+		private readonly List<string> AdditionalNotesStyleList = new List<string>(5)
+		{
+			"(Note)",
+			"[Note]",
+			"{Note}",
+			"- Note",
+			"Note"
+		};
+	}
+}
---
+++ NotesPlus/Utils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using BetterTOS2;
+using Server.Shared.State;
+using Services;
+using SML;
+
+namespace NotesPlus
+{
+	// Token: 0x02000006 RID: 6
+	public static class Utils
+	{
+		// Token: 0x06000026 RID: 38 RVA: 0x0000226A File Offset: 0x0000046A
+		public static bool BTOS2Exists()
+		{
+			return ModStates.IsEnabled("curtis.tuba.better.tos2");
+		}
+
+		// Token: 0x06000027 RID: 39 RVA: 0x0000477C File Offset: 0x0000297C
+		public static bool IsBTOS2()
+		{
+			bool result;
+			try
+			{
+				result = Utils.IsBTOS2Bypass();
+			}
+			catch
+			{
+				result = false;
+			}
+			return result;
+		}
+
+		// Token: 0x06000028 RID: 40 RVA: 0x00002276 File Offset: 0x00000476
+		private static bool IsBTOS2Bypass()
+		{
+			return Utils.BTOS2Exists() && BTOSInfo.IS_MODDED;
+		}
+
+		// Token: 0x06000029 RID: 41 RVA: 0x00002286 File Offset: 0x00000486
+		public static bool IsPandora()
+		{
+			return Utils.IsBTOS2() && Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains((Role)222);
+		}
+
+		// Token: 0x0600002A RID: 42 RVA: 0x000022B9 File Offset: 0x000004B9
+		public static bool IsCompliance()
+		{
+			return Utils.IsBTOS2() && Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains((Role)221);
+		}
+
+		// Token: 0x0600002B RID: 43 RVA: 0x000022EC File Offset: 0x000004EC
+		public static bool IsCTT()
+		{
+			return Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains(Role.TOWN_TRAITOR);
+		}
+
+		// Token: 0x0600002C RID: 44 RVA: 0x00002316 File Offset: 0x00000516
+		public static bool IsATT()
+		{
+			return Utils.IsBTOS2() && Service.Game.Sim.simulation.roleDeckBuilder.Data.modifierCards.Contains(Role.FOUR_HORSEMEN);
+		}
+	}
+}
